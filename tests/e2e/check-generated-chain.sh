7b789a15d2ff06b4d654e387fda238fbd1a15dac
Christopher Albert <albert@tugraz.at>
2026-08-23 22:14:32 +0200

    Use range expansion for variable print routes

--- tests/e2e/check-generated-chain.sh
+++ tests/e2e/check-generated-chain.sh
@@ -1181,7 +1181,7 @@ if python3 "$oracle" "$print_variable_power_value_ast_file" \
     exit 1
 fi
 
-for variable_output_count in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100; do
+for variable_output_count in {2..100}; do
     case "$variable_output_count" in
         2) variable_output_word=two; variable_output_ordinal=second ;;
         3) variable_output_word=three; variable_output_ordinal=third ;;
